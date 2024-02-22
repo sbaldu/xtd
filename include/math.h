23b152e277dab17bb170edc7bed1df225eabb6e9
Andrea Bocci <andrea.bocci@cern.ch>
2024-02-22 09:46:33 +0100

    Implement xtd::sin() and xtd::sinf()

---
+++ include/math.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2024 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#pragma once
+
+#include "math/sin.h"
